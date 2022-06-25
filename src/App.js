0b1dd1ae672026d1d79b53732b4067506a3e982b
RossCr0ss <sobechko.r@gmail.com>
2022-06-25 14:49:25 +0300

    Adding a Users List Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
 const App = () => {
   return (
     <>
       <AddUser />
+      <UsersList users={[{ name: 'Ross', age: 27 }]} />
     </>
   );
 };
